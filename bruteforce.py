d2afe726e1413f90cef1ff8baae611081ef1d815
MDCarrasco <MDCarrasco@users.noreply.github.com>
2021-03-03 13:46:04 +0100

    first commit

---
+++ bruteforce.py
@@ -0,0 +1,38 @@
+# bruteforce.py
+# Created at: Wed Mar 03 2021 13:45:39 GMT+0100 (Central European Standard Time)
+# Copyright 2021 MDCarrasco <michaeldanielcarrasco@gmail.com>
+#
+
+"""
+bruteforce.py
+INSERT docstring paragraph
+
+Example:
+        INSERT example
+
+Todo:
+        * INSERT TODO lines
+        *
+
+.. _Google Python Style Guide (reference):
+http://google.github.io/styleguide/pyguide.html
+"""
+
+# Futures
+
+# Generic/Built-in
+
+# Other Libs
+
+# Owned
+
+__author__ = "Michael Carrasco"
+__copyright__ = "2021 MDCarrasco <michaeldanielcarrasco@gmail.com>"
+__credits__ = ["Michael Carrasco"]
+__license__ = ""
+__version__ = "0.0.1"
+__maintainer__ = "Michael Carrasco"
+__email__ = "<michaeldanielcarrasco@gmail.com>"
+__status__ = "Dev"
+
+
